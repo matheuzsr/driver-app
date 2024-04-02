e9f9f548cdd982196b4910744d3089f56194de7f
Matheus Rocha <matheusdasilvarocha@gmail.com>
2024-04-02 10:49:48 -0300

    fix: change dir from config

--- src/database/dao/AccountDAOPostgres.ts
+++ src/database/dao/AccountDAOPostgres.ts
@@ -1,6 +1,6 @@
 import pgp from 'pg-promise'
 import { type Person } from '../../dto/PersonDto'
-import config from '../../config'
+import config from '../../config/config'
 
 export default class AccountDAOPostgres {
   private readonly uriDB = `postgres://${config.DB_USER}:${config.DB_PASSWORD}@${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`
